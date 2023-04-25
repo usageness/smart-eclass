f9bb812b48127565f61590de9545f068532ce5c9
YongRae_Kim (Usage) <kyr9389@naver.com>
2023-04-26 02:30:25 +0900

    chore: 서버 <-> 데이터베이스 연동

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,33 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { StudyModule } from './study/study.module';
+import { User } from './users/entities/user.entity';
+import { Study } from './study/entities/study.entity';
 
 @Module({
-  imports: [UsersModule, StudyModule],
+  imports: [
+    UsersModule,
+    StudyModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [User, Study],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
